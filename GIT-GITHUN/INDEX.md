3dbc0c59ea7ca297b84f7f30ddf8b142a85ab1c9
Richierichman15 <richierichman15@gmail.com>
2022-12-14 09:32:34 -0600

    Added Git Commands on index.md

---
+++ GIT-GITHUN/INDEX.md
@@ -0,0 +1,15 @@
+# GIT Commands
+
+* git init
+    * allows us to use git commands
+    * creates the .git folder
+
+* git status
+    * shows files that have been changed
+    * shows files that need to be added
+
+git add
+    - add the file to the staging
+
+* git commit
+    * adds the file to the history
--- warmups/index.js
+++ warmups/index.js
@@ -1,97 +1,27 @@
-// const firstname = 'Josh';
-// const lastName = 'Picallo';
-// let age = 90;
-// let  isMarried = false;
-// let  pets = null;
 
-// if(isMarried) {
-//     const spouseFirstName = 'Jennifer';
-//     let spouseLastName ='Truman';
+// fav function to return which input is bigger
+const betterFav = (myFav, yourFav) => {
+    
+return typeof myFav === "string" ? 
+    (myFav.length > yourFav.length ? myFav : yourFav)  : 
+    (myFav > yourFav ? myFav : yourFav) ;
 
-//     spouseLastName = lastName;
-//     console.log(`${spouseFirstName} ${spouseLastName}`);
-// } else if (age < 55) {
-//     pets = 'Randy the dog and Pete the cat';
-//     console.log(pets);
-// }   else if (firstName[0] === 'J') {
-//     isMarried = true;
-//     console.log(isMarried);
-// } else {
-//     age = 85;
-//     console.log(age);
-// }
+    // are the inputs strings or numbers
+    // if(typeof myFav === "string") {
+    // if given 2 strings return the longer string
+    // if(myFav.length > yourFav.length) {
+    //     return myFav;
+    // } else {
+    //     return yourFav;
+    // }
 
-// if(age > 67) {
-//     console.log(`${firstName} is ${age} years old`)
-// }
+        //  myFav.length > yourFav.length ? myFav : yourFav;
+    // }
 
-// const string54ToNumber = '54'
-// for(let i = 0; i < 6; i++) {
-
-// }
-
-// const firstname = 'Posh';
-// const lastName = 'Picallo';
-// let age = 60;
-// let  isMarried = false;
-// let  pets = null;
-
-// if(isMarried === true && age > 89 || pets === null) {
-//     console.log('truthy');
-// } else {
-//     console.log('falsey');
-// }
-
-
-
-// const isTall = true;
-// const hasSpouse = true;
-// // ternary
-// const isMarried = hasSpouse ? 'Robbins' : 'Haroldson'; 
-// console.log(lastName);
-
-
-
-// const sum = (num1, num2) => {
-//     console.log('inside the function');
-//   return num1 + num2;
-// }
-
-// const subtract = (num1, num2) => {
-//     const favNumber = 22;
-
-
-//     return favNumber - num1 - num2;
-// }
-
-// console.log(subtract(2, 4));
-// console.log(subtract(7, 13));
-
-
-// create a function called fullName
-// return the fullName of a person
-// takes in a firstName and a lastName
-// if no firstName is given, make the firstName joe
-// if no lastName is given, make the lastName Buckley
-// if neither firstName nor lastName are given return Becky Bleu
-
-
-
-const fullName = (firstName, lastName) => {
-if(!firstName) {
-    firstName = 'Joe';
-} 
-
-if(!lastName) {
-
-    lastName = 'Buckley';
+    // if given 2 numbers return the bigger number
+    // return myFav > yourFav ? myFav : yourFav;
+    // return condition ? if true : if false
 }
 
-if (!firstName && !lastName) {
-    return 'Becky Bleu';
-}
-    return `${firstName} ${lastName}`;
-
-}
-
-console.log(fullName('', ''));
+// console.log(betterFav(1,4));
+console.log(betterFav('Bunnies', 'Dog'));
